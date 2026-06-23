8b9051045486d10da8b08bc339a7789ef0365f2a
liam <liam@scalzulli.com>
2026-06-23 00:52:48 -0400

    Use inventory for rule registration (#134)

--- src/rule.rs
+++ src/rule.rs
@@ -29,6 +29,8 @@ macro_rules! define_rule {
         $body
       }
     }
+
+    inventory::submit!(&$name as &dyn Rule);
   };
   (@default $level:expr) => {
     Some($level)
@@ -38,34 +40,7 @@ macro_rules! define_rule {
   };
 }
 
-pub(crate) use {
-  dependency_groups::DependencyGroupsRule,
-  project_classifiers::ProjectClassifiersRule,
-  project_dependencies::ProjectDependenciesRule,
-  project_dependencies_version_bounds::ProjectDependenciesVersionBoundsRule,
-  project_dependency_deprecations::ProjectDependencyDeprecationsRule,
-  project_dependency_updates::ProjectDependencyUpdatesRule,
-  project_description::ProjectDescriptionRule,
-  project_dynamic::ProjectDynamicRule,
-  project_entry_points::ProjectEntryPointsRule,
-  project_entry_points_extras::ProjectEntryPointsExtrasRule,
-  project_import_names::ProjectImportNamesRule,
-  project_keywords::ProjectKeywordsRule,
-  project_license_classifiers::ProjectLicenseClassifiersRule,
-  project_license_classifiers_deprecated::ProjectLicenseClassifiersDeprecatedRule,
-  project_license_files::ProjectLicenseFilesRule,
-  project_license_value::ProjectLicenseValueRule,
-  project_license_value_deprecations::ProjectLicenseValueDeprecationsRule,
-  project_name::ProjectNameRule,
-  project_optional_dependencies::ProjectOptionalDependenciesRule,
-  project_people::ProjectPeopleRule, project_readme::ProjectReadmeRule,
-  project_readme_content_type::ProjectReadmeContentTypeRule,
-  project_requires_python::ProjectRequiresPythonRule,
-  project_requires_python_upper_bound::ProjectRequiresPythonUpperBoundRule,
-  project_unknown_keys::ProjectUnknownKeysRule, project_urls::ProjectUrlsRule,
-  project_version::ProjectVersionRule, schema::SchemaRule,
-  semantic::SemanticRule, syntax::SyntaxRule,
-};
+pub(crate) use schema::SchemaRule;
 
 mod dependency_groups;
 mod project_classifiers;
@@ -97,6 +72,8 @@ mod project_version;
 mod schema;
 mod semantic;
 mod syntax;
+
+inventory::collect!(&'static dyn Rule);
 
 pub(crate) trait Rule: Sync {
   /// The default severity level for the rule when not configured.
